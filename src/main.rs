22dc13355fb308a1536a63e1565e264ac4016f43
milomilo33 <milovanovicm309@gmail.com>
2023-02-04 02:22:28 +0100

    Parallel implementation w/ mutexes

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::hash::Hash;
 use std::io::{ self, BufRead, BufReader };
 use std::process::exit;
 use std::collections::{ HashSet, VecDeque };
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 #[derive(Clone, PartialEq, Debug)]
 enum Direction {
@@ -48,6 +50,7 @@ impl MazeCell {
 //     ends: Vec<(u32, u32)>,
 // }
 
+#[derive(Clone)]
 struct MazeState {
     current_position: (usize, usize),
     previous_positions: Vec<(usize, usize)>,
@@ -248,14 +251,154 @@ fn solve_maze_bfs(maze_table: Array2D<MazeCell>, initial_maze_state: MazeState) 
     match maze_end_state {
         Some(maze_end_state) => {
             // form & draw solution output
-            write_and_draw_solution(&maze_end_state, &maze_table)
+            write_and_draw_solution(&maze_end_state, &maze_table, String::from("sequential"));
         },
         None => return
     }
 }
 
-fn write_and_draw_solution(maze_end_state: &MazeState, maze_table: &Array2D<MazeCell>) {
-    println!("(row, col) indexes of solution in order:\n");
+fn get_new_state_if_neighbour_valid(maze_state: &MazeState, current_cell: &MazeCell, direction: Direction) -> Option<MazeState> {
+    let neighbour_position: (usize, usize);
+    let opposite_direction: Direction;
+    match direction {
+        Direction::WEST => {
+            neighbour_position = (current_cell.row_index, current_cell.col_index - 1);
+            opposite_direction = Direction::EAST;
+        },
+        Direction::EAST => { 
+            neighbour_position = (current_cell.row_index, current_cell.col_index + 1);
+            opposite_direction = Direction::WEST;
+        },
+        Direction::NORTH => { 
+            neighbour_position = (current_cell.row_index - 1, current_cell.col_index);
+            opposite_direction = Direction::SOUTH;
+        },
+        Direction::SOUTH => { 
+            neighbour_position = (current_cell.row_index + 1, current_cell.col_index);
+            opposite_direction = Direction::NORTH;
+        },
+    }
+
+    let mut neighbour_state = MazeState::new();
+    neighbour_state.current_position = neighbour_position;
+    neighbour_state.cells_with_locked_doors = maze_state.cells_with_locked_doors.clone();
+    neighbour_state.num_keys_to_use = maze_state.num_keys_to_use;
+
+    // check door and unlock (from both sides) if needed
+    if maze_state.cells_with_locked_doors.contains(&(maze_state.current_position.0, maze_state.current_position.1, direction.clone())) {
+        // decrement num of keys and unlock door for next state
+        if maze_state.num_keys_to_use > 0 {
+            neighbour_state.num_keys_to_use -= 1;
+            let current_cell_with_locked_door = (maze_state.current_position.0, maze_state.current_position.1, direction.clone());
+            let neighbour_cell_with_locked_door = (neighbour_position.0, neighbour_position.1, opposite_direction);
+            neighbour_state.cells_with_locked_doors.retain(|x| *x != current_cell_with_locked_door && *x != neighbour_cell_with_locked_door);
+        }
+        // no available keys, so neighbour is not valid
+        else {
+            return None;
+        }
+    }
+
+    neighbour_state.keys_left = maze_state.keys_left.clone();
+
+    // pick up key in neighbour cell if available
+    if maze_state.keys_left.contains(&neighbour_position) {
+        neighbour_state.num_keys_to_use += 1;
+        neighbour_state.keys_left.retain(|x| *x != neighbour_position);
+    }
+
+    neighbour_state.previous_positions = maze_state.previous_positions.clone();
+    neighbour_state.previous_positions.push(maze_state.current_position);
+
+    return Some(neighbour_state);
+}
+
+fn solve_maze_bfs_parallel(maze_table: Array2D<MazeCell>, initial_maze_state: MazeState) {
+    // let maze_table_x = Arc::new(Box::new(maze_table));
+
+    // cells visited while having visited[2] keys available
+    let visited: Arc<Mutex<HashSet<(usize, usize, u32)>>> = Arc::new(Mutex::new(HashSet::new()));
+    {
+        visited.lock().unwrap().insert((initial_maze_state.current_position.0, initial_maze_state.current_position.1, initial_maze_state.num_keys_to_use));
+    }
+
+    let bfs_queue: Arc<Mutex<VecDeque<MazeState>>> = Arc::new(Mutex::new(VecDeque::new()));
+    {
+        bfs_queue.lock().unwrap().push_back(initial_maze_state);
+    }
+
+    let mut maze_end_state: Option<MazeState> = None;
+
+    // let critical_section = Arc::new(Mutex::new(()));
+
+    // let maze_table = Arc::new(maze_table);
+    loop {
+        {
+            if bfs_queue.lock().unwrap().is_empty() {
+                break;
+            }
+        }
+        let current_maze_state: Arc<MazeState>;
+        {
+            current_maze_state = Arc::new(bfs_queue.lock().unwrap().pop_front().unwrap());
+        }
+        
+        // let maze_table = Arc::new(&maze_table);
+        let current_position = current_maze_state.current_position;
+        let current_maze_cell = Arc::new(maze_table.get(current_position.0, current_position.1).unwrap().clone());
+
+        // found end of maze
+        if current_maze_cell.end_of_maze {
+            match Arc::try_unwrap(current_maze_state) {
+                Ok(current_maze_state) => maze_end_state = Some(current_maze_state),
+                Err(_) => maze_end_state = None,
+            }
+            break;
+        }
+
+        let mut spawned_threads = Vec::new();
+        // let critical_section = Arc::clone(&critical_section);
+           
+        for direction in current_maze_cell.available_directions.clone() {
+            let visited = Arc::clone(&visited);
+            let bfs_queue = Arc::clone(&bfs_queue);
+            let current_maze_state = Arc::clone(&current_maze_state);
+            let current_maze_cell = Arc::clone(&current_maze_cell);
+            // let maze_table = Arc::clone(&maze_table);
+            let thread = thread::spawn(move || {
+                let neighbour_state = get_new_state_if_neighbour_valid(&current_maze_state, &current_maze_cell, direction.clone());
+
+                match neighbour_state {
+                    Some(neighbour_state) => {
+                        let mut visited_guard = visited.lock().unwrap();
+                        // ignore neighbour if it has been visited with the same number of keys available
+                        if !visited_guard.contains(&(neighbour_state.current_position.0, neighbour_state.current_position.1, neighbour_state.num_keys_to_use)) {
+                            visited_guard.insert((neighbour_state.current_position.0, neighbour_state.current_position.1, neighbour_state.num_keys_to_use));
+                            bfs_queue.lock().unwrap().push_back(neighbour_state);
+                        }
+                    },
+                    None => (),
+                }
+            });
+            spawned_threads.push(thread);
+        }
+
+        for thread in spawned_threads {
+            thread.join().unwrap();
+        }
+    }
+
+    match maze_end_state {
+        Some(maze_end_state) => {
+            // form & draw solution output
+            write_and_draw_solution(&maze_end_state, &maze_table, String::from("parallel"));
+        },
+        None => return
+    }
+}
+
+fn write_and_draw_solution(maze_end_state: &MazeState, maze_table: &Array2D<MazeCell>, keyword: String) {
+    println!("(row, col) indexes of {} solution in order:\n", keyword);
     let mut iter = 1;
     for position in &maze_end_state.previous_positions {
         println!("{}. ({}, {})", iter, position.0, position.1);
@@ -263,7 +406,7 @@ fn write_and_draw_solution(maze_end_state: &MazeState, maze_table: &Array2D<Maze
     }
     println!("{}. ({}, {})", iter, maze_end_state.current_position.0, maze_end_state.current_position.1);
 
-    println!("\nEnd of solution.\n\nTable representation of solution (0 = untraversed; 1 = traversed):\n");
+    println!("\nEnd of {} solution.\n\nTable representation of solution (0 = untraversed; 1 = traversed):\n", keyword);
     // let mut row_iter = 0;
     // let mut col_iter = 0;
     for iterator in maze_table.rows_iter() {
@@ -338,6 +481,8 @@ fn draw_initial_maze(initial_maze_state: &MazeState, maze_table: &Array2D<MazeCe
 
 fn main() {
     let (maze_table, initial_maze_state) = read_maze_from_file(String::from("maze_def.txt"));
+    let maze_table_parallel = maze_table.clone();
+    let initial_maze_state_parallel = initial_maze_state.clone();
 
     // for locked in &initial_maze_state.cells_with_locked_doors {
     //     println!("({}, {}, {:?})", locked.0, locked.1, locked.2);
@@ -345,5 +490,19 @@ fn main() {
 
     draw_initial_maze(&initial_maze_state, &maze_table);
 
+    use std::time::Instant;
+    let now = Instant::now();
+
     solve_maze_bfs(maze_table, initial_maze_state);
+
+    let elapsed = now.elapsed();
+    println!("Elapsed (sequential): {:.2?}", elapsed);
+
+
+    let now = Instant::now();
+
+    solve_maze_bfs_parallel(maze_table_parallel, initial_maze_state_parallel);
+
+    let elapsed = now.elapsed();
+    println!("Elapsed (parallel): {:.2?}", elapsed);
 }
